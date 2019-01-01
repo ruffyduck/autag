e756acc3be55e89c13b138f7adf74d130f1b34cb
Rafael Theis <rafael_ths@yahoo.de>
2019-01-01 17:56:57 +0100

    Added ability to read command line arguments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Entry point for AuTag"""
 
+import sys
 from controller.auclean import AutoCleanup
 from controller.aucrawl import AutoCrawler
 from controller.audefault import AutoDefault
@@ -15,21 +16,26 @@ from model.basetag import get_tag
 from view.augui import AuGUI
 
 
-AUDICTS = build_auto_dicts("resources/autodicts.json")
+def main(argv):
+	AUDICTS = build_auto_dicts("resources/autodicts.json")
+	AUTO_TAG = AutoTagger()
+	AUTO_TAG.add_auto_tags([AutoImage(), AutoCleanup(), AutoNumbers(),
+	                        AutoCrawler("UVtLSggkngYRiFHyFSdnjrgaFufcXmWIjrhiPkiN",
+	                                    [get_tag("GENRE"),
+	                                     get_tag("ORGANIZATION")],
+	                                    AUDICTS),
+	                        ReplaceTag(AUDICTS["GENRE"], get_tag("GENRE")),
+	                        ReplaceTag(AUDICTS["TITLE"], get_tag("TITLE")),
+	                        AutoRemoval("resources/autoremove.json"),
+	                        AutoDefault("resources/defaultvalues.json")])
 
-AUTO_TAG = AutoTagger()
-AUTO_TAG.add_auto_tags([AutoImage(), AutoCleanup(), AutoNumbers(),
-                        AutoCrawler("UVtLSggkngYRiFHyFSdnjrgaFufcXmWIjrhiPkiN",
-                                    [get_tag("GENRE"),
-                                     get_tag("ORGANIZATION")],
-                                    AUDICTS),
-                        ReplaceTag(AUDICTS["GENRE"], get_tag("GENRE")),
-                        ReplaceTag(AUDICTS["TITLE"], get_tag("TITLE")),
-                        AutoRemoval("resources/autoremove.json"),
-                        AutoDefault("resources/defaultvalues.json")])
+	AUTO_FILE = AutoTagger()
+	AUTO_FILE.add_auto_tags([AutoRename()])  #, AutoMove("D:/Musik/")])
 
-AUTO_FILE = AutoTagger()
-AUTO_FILE.add_auto_tags([AutoRename()])  #, AutoMove("D:/Musik/")])
+	gui = AuGUI(AUTO_TAG, AUTO_FILE)
+	gui.draw()
 
-gui = AuGUI(AUTO_TAG, AUTO_FILE)
-gui.draw()
+
+
+if __name__== "__main__":
+	main(sys.argv);
